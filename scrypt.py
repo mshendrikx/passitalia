a47829d4ef949618af7eeddd1523e9f683b12ce4
Maurício Hendrikx <mauricio.servatius@gmail.com>
2026-06-23 10:15:42 -0300

    main

--- scrypt.py
+++ scrypt.py
@@ -1,6 +1,7 @@
 import os
 import time
 import random
+import logging
 
 from seleniumbase import SB
 from whatsapp_api import whatsapp_send_message, whatsapp_get_numberid
@@ -12,6 +13,12 @@ WHATSAPP_NUMBER = os.environ.get("WHATSAPP_NUMBER")
 REFRESH_RATE_MIN = int(os.environ.get("REFRESH_RATE_MIN", 60))
 REFRESH_RATE_MAX = int(os.environ.get("REFRESH_RATE_MAX", 120))
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", filename="/app/log/passitalia.log", filemode="a")
+logger = logging.getLogger(__name__)
+
+print("Starting SeleniumBase script")
+logger.info("Starting SeleniumBase script")
+
 with SB(
     # browser="chrome",
     headless=True,
@@ -19,6 +26,9 @@ with SB(
     servername=os.environ.get("SELENIUM_HUB_HOST", None),
     port=os.environ.get("SELENIUM_HUB_PORT", None),
 ) as sb:
+    
+    print("SeleniumBase initialized successfully")
+    logger.info("SeleniumBase initialized successfully")
 
     login = False
 
@@ -34,13 +44,14 @@ with SB(
             )
             
             if whatsapp_id is None:
-                print("Error retrieving whatsapp number id")
+                logger.error("Error retrieving whatsapp number id")
                 time.sleep(60)
                 continue
 
             contacts = [whatsapp_id]
             content = "Agendamento disponível! Acesse https://prenotami.esteri.it/ para marcar seu horário."
-            print("Sucessfully retrieved whatsapp number id")
+            logger.info("Successfully retrieved whatsapp number id")
+            print("Successfully retrieved whatsapp number id")
 
             sb.open("https://prenotami.esteri.it/")
             sb.click("/html/body/header/nav[1]/div/div/a[5]")
@@ -48,10 +59,12 @@ with SB(
             sb.type('input[id="floatingLabelInput33"]', os.environ.get("EMAIL"))
             sb.type('input[id="floatingLabelInput38"]', os.environ.get("PASSWORD"))
             sb.click('//*[@id="wrapper"]/div[3]/button')
-            print("Sucessfully logged in")
+            logger.info("Successfully logged in")
+            print("Successfully logged in")
             login = True
         except Exception as e:
-            print("Error during login:", e)
+            logger.error("Error during login: %s", e)
+            print("Error during login: %s" % e)
 
     while 1 == 1:
 
@@ -65,12 +78,14 @@ with SB(
                 "/html/body/div[2]/div[2]/div/div/div/div/div/div"
             )
             if "All appointments for this service are currently booked" in message.text:
+                logger.info("No appointments available")
                 print("No appointments available")
                 sb.click(
                     "/html/body/div[2]/div[2]/div/div/div/div/div/div/div/div[4]/button"
                 )
 
             else:
+                logger.info("Appointments available")
                 print("Appointments available")
                 whatsapp_send_message(
                     base_url=WHATSAPP_BASE_URL,
@@ -86,11 +101,13 @@ with SB(
                 "/html/body/div[2]/div[2]/div/div/div/div/div/div"
             )
             if "All appointments for this service are currently booked" in message.text:
+                logger.info("No appointments available")
                 print("No appointments available")
                 sb.click(
                     "/html/body/div[2]/div[2]/div/div/div/div/div/div/div/div[4]/button"
                 )
             else:
+                logger.info("Appointments available")
                 print("Appointments available")
                 whatsapp_send_message(
                     base_url=WHATSAPP_BASE_URL,
@@ -102,7 +119,8 @@ with SB(
                 )
 
         except Exception as e:
-            print(e)
+            logger.error("Error occurred: %s", e)
+            print("Error occurred: %s" % e)
 
         interval = random.randint(REFRESH_RATE_MIN, REFRESH_RATE_MAX)
         time.sleep(interval)
